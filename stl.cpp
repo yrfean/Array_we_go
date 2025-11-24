6fc749436e1be337e7a858d631179497d8ae414f
Irfan Ashraf <irfanshalu3@gmail.com>
2025-11-24 15:27:02 +0530

    day-2 pf kali polli

---
+++ stl.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // LIST:
+    // list is easy just same as vector
+
+    // list <int> ls;
+    // ls.push_back(10);
+    // ls.push_front(15);
+    // ls.push_back(20);
+
+
+    // STACK:
+    // stack <int> st;
+    // st.push(10);
+    // st.push(20);
+    // st.emplace(30);
+
+    // st.pop(); //removes 30
+    // cout << st.top(); //20
+
+    // cout << st.size();
+    // cout << st.empty();
+
+    // QUEUE
+    // opos of stack (first-come,first serve)
+
+    // queue<int> q;
+    // q.push(10);
+    // q.push(20);
+    // q.emplace(30);
+
+    // q.pop(); //removes 10
+    // q.back() +=5; //30->35
+
+    // cout << q.front();
+
+    // PRIORITY_QUEUE:
+    // elemenst are stored in order of largest-smallest
+    // alsmost same as stack--pop() will remove top one
+
+
+    // SET:
+    // 1-ordered 2-unique
+
+    // set <int> s;
+    // s.insert(1);
+    // s.insert(2);
+    // s.insert(3);
+    // s.emplace(3);
+
+    // auto it = s.find(3);
+
+    // s.erase(it);
+    // s.erase(5);
+
+    // s.count(2)
+
+    // MAP:
+    //
+
+    return 0;
+}
