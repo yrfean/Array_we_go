ca3c7cd9bc9428bcdf70814c1efd8c3725e40711
Irfan Ashraf <irfanshalu3@gmail.com>
2025-11-27 20:35:30 +0530

    it didnt rain today

--- recursion/recursion1.cpp
+++ recursion/recursion1.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 using namespace std;
 
-void run(){
-    if(count==5)return;
-    cout << "yrfan ashraf";
-    count ++;
-    run();
+void prob1(int i,int n){
+    if(i>n)return;
+    cout << "yrfan ashraf"<<endl;
+    prob1(i+1,n);
 }
-int count = 0;
+
+void prob2(int i,int m){
+    if(i>m)return;
+    cout <<i<<" ";
+    prob2(i+1,m);
+}
+
+void prob3(int i){
+    if(i<0)return;
+    cout << i<<" ";
+    prob3(i-1);
+}
+
+
 
 int main() {
+    // prob 1
+    // int n ;
+    // cout << "how many times u want to out?"<< endl;
+    // cin >> n;
+    // prob1(1,n);
 
-    run();
 
-    cout << "end" << endl;
+    // prob 2
+    prob2(1,5);
+
+    // prob 3
+    // prob3(5);
+
+    // Backtracking:
+    // bruh backtracking is just letting it print after all functiosn are called its coming back-
+    // to erase all functions
+
+
     return 0;
 }
---
+++ recursion/recursion2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sum(int i,int n){
+    if(i<0)return n;
+    int res=n;
+    return sum(i-1,n+i);
+}
+
+int main() {
+    // SUM OF FIRST N NUMBERS:
+    cout << sum(5,0);
+    return 0;
+}
