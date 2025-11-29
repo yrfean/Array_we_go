6ba073d8c9ad03377db9b08b0e1b43b62f12b8c3
Irfan Ashraf <irfanshalu3@gmail.com>
2025-11-29 11:41:17 +0530

    thinking of meeting binj and arfi tuesday but idk what to do or where we should go

--- recursion/recursion2.cpp
+++ recursion/recursion2.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int sum(int i,int n){
-    if(i<0)return n;
-    return sum(i-1,n+i);
+void para(int i,int n){
+    if(i==0){
+        cout << n;
+        return;
+    }
+    para(i-1,n+i);
+}
+
+int fun(int n){
+    if(n==0){
+        return n;
+    }
+    return n+ fun(n-1);
+}
+
+void paraf(int i,int n){
+    if(i==0){
+        cout << n;
+        return;
+    }
+    paraf(i-1,n*i);
+}
+
+int funf(int n){
+    if(n==1){
+        return n;
+    }
+    return n* funf(n-1);
 }
 
 int main() {
     // SUM OF FIRST N NUMBERS:
-    cout << sum(5,0);
+    // para way:
+    // para(5,0);
+
+    // fuinctional way:
+
+    // cout << fun(5);
+
+    // Factorial :
+    // para way:
+    // paraf(5,1);
+
+    // fun way:
+    cout << funf(5);
     return 0;
 }
